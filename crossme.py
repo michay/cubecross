c84f9a5581bad4092e5ab7f35f44cf82db76443d
myonin <micha.yonin@sandisk.com>
2021-08-09 17:30:47 +0300

    removed axis rotation; manipulating regular rotations to somehow get it to work

--- crossme.py
+++ crossme.py
@@ -8,8 +8,15 @@ COLOR_YELLOW = 5
 COLOR_COUNT = 6
 
 def color_to_text(color):
+
     colors_array = ['B', 'W', 'G', 'O', 'R', 'Y']
-    return colors_array[color]
+    if not CSide.is_debug_mode:
+        return colors_array[color]
+    else:
+        #if color % 10 == 0:
+            #return '%s%d' % (colors_array[color/10 - 1], color/10)
+        #return str(color)
+        return '%s%d' % (colors_array[color/10 - 1], color%10)
 
 class CSide(object):
 
@@ -19,21 +26,46 @@ class CSide(object):
     c3 e2 c2
     '''
     
+    is_debug_mode = False
+    
     def __init__(self, color):
         
-        self.center = color
-        self.edges = [color] * 4
-        self.corners = [color] * 4
+        if not CSide.is_debug_mode:
+            self.center = color
+            self.edges = [color] * 4
+            self.corners = [color] * 4
+        else:
+            color += 1
+            self.center = color * 10
+            self.edges = [color*10 + i*2 + 2 for i in xrange(4)]
+            self.corners = [color*10 + i*2 + 1 for i in xrange(4)]
+        self.is_back_side = False
+        
+    def update_back_side(self):
+        self.is_back_side = True
         
     def __repr__(self):
         result = []
-        result.append([self.corners[0], self.edges[0], self.corners[1]])
-        result.append([self.edges[3], self.center, self.edges[1]])
-        result.append([self.corners[3], self.edges[2], self.corners[2]])
-        
+        if not self.is_back_side:
+            result.append([self.corners[0], self.edges[0], self.corners[1]])
+            result.append([self.edges[3], self.center, self.edges[1]])
+            result.append([self.corners[3], self.edges[2], self.corners[2]])
+        else:
+            result.append([self.corners[1], self.edges[0], self.corners[0]])
+            result.append([self.edges[1], self.center, self.edges[3]])
+            result.append([self.corners[2], self.edges[2], self.corners[3]])
+            
         return '\n'.join([' '.join(color_to_text(x) for x in rrr) for rrr in result])
         
     def get_side(self, side):
+        
+        '''
+        if self.is_back_side:
+            if side == 'L':
+                side = 'R'
+            elif side == 'R':
+                side = 'L'
+        '''
         
         if side == 'U':
             return [self.corners[0], self.edges[0], self.corners[1]]
@@ -47,14 +79,23 @@ class CSide(object):
         assert False
         
     def update_side(self, side, new_vals):
+        '''
+        if self.is_back_side:
+            if side == 'L':
+                side = 'R'
+            elif side == 'R':
+                side = 'L'
+        '''
         if side == 'U':
             [self.corners[0], self.edges[0], self.corners[1]] = new_vals
-        if side == 'D':
+        elif side == 'D':
             [self.corners[3], self.edges[2], self.corners[2]] = new_vals
-        if side == 'L':
+        elif side == 'L':
             [self.corners[0], self.edges[3], self.corners[3]] = new_vals
-        if side == 'R':
+        elif side == 'R':
             [self.corners[1], self.edges[1], self.corners[2]] = new_vals
+        else:
+            assert False
         
         
     def rotate(self, is_clockwise):
@@ -71,7 +112,7 @@ class CSide(object):
             x = self.corners.pop(0)
             self.corners.append(x)
 
-class CRubiks(object):
+class CCube(object):
     
     '''
       B
@@ -87,6 +128,8 @@ class CRubiks(object):
             self.sides.append(CSide(i))
         self.init_sides_hash()
         
+        self.sides[self.sides_hash['B']].update_back_side()
+        
     def init_sides_hash(self):
         self.sides_hash['B'] = COLOR_BLUE
         self.sides_hash['F'] = COLOR_GREEN
@@ -95,40 +138,6 @@ class CRubiks(object):
         self.sides_hash['R'] = COLOR_RED
         self.sides_hash['D'] = COLOR_YELLOW
         
-    
-    def rotate_axis(self, axis):
-        
-        flow = ''
-        rotate_me = ''
-        is_clockwise = True
-        is_anti = False
-        if axis[0] == 'Y':
-            flow = 'FLBR'
-            rotate_me = ["U'", "D"]
-
-        if axis[0] == 'Z':
-            flow = 'LURD'
-            rotate_me = ["F'", "B"]
-        
-        if axis[-1] == "'":
-            flow = flow[::-1]
-            is_anti = True
-
-        for rr in rotate_me:
-            side = rr[0]
-            is_clockwise  = rr[-1] == "'"
-            if is_anti:
-                is_clockwise = not is_clockwise
-            self.sides[self.sides_hash[side]].rotate(is_clockwise)
-            
-        assert len(flow)
-        flow = flow[-1] + flow
-        new_val = self.sides[self.sides_hash[flow[0]]]
-        for i in xrange(len(flow) - 1):
-            old_val = self.sides[self.sides_hash[flow[i+1]]]
-            self.sides[self.sides_hash[flow[i+1]]] = new_val
-            new_val = old_val
-            
     def rotate(self, action):
         vals = action.split(' ')
         print vals
@@ -137,45 +146,39 @@ class CRubiks(object):
             if action[-1] == '2':
                 action = action[:-1]
                 repeat_me = 2
-            
-            print ('%s%d' % (action, repeat_me)),
+
             for i in xrange(repeat_me):
                 self.rotate_single(action)
         print ''
+    
+    def translate_rotate_action(self, action, flow):
+        if action =='F':
+            reverse_hash = {'U': 'D', 'D': 'U', 'L': 'R', 'R': 'L'}
+            return reverse_hash[flow]
+        elif action == 'B':
+            reverse_hash = {'U': 'U', 'D': 'D', 'L': 'L', 'R': 'R'}
+            return reverse_hash[flow]
+        else:
+            return action
     
     def rotate_single(self, action):
     
         is_clockwise = len(action) == 1
         action = action[0]
-        rotated = ''
-        
-        if action == 'F':
-            rotated = 'Y'
-            action = 'L'
-            
-        elif action == 'B':
-            rotated = 'Y'
-            action = 'R'
-
-        elif action == 'U':
-            rotated = 'Z'
-            action = 'R'
-
-        elif action == 'D':
-            rotated = 'Z'
-            action = 'L'
-
-        if rotated:
-            self.rotate_axis(rotated)
 
         side = self.sides_hash[action]
-        
         self.sides[side].rotate(is_clockwise)
-        
         is_opposite = False
+        
         if action == 'R' or action == 'L':
             flow = 'UBDF'
             is_opposite = (action == 'R' and not is_clockwise) or (action == 'L' and is_clockwise)
+        elif action == 'U' or action == 'D':
+            flow = 'FLBR'
+            is_opposite = (action == 'U' and not is_clockwise) or (action == 'D' and is_clockwise)
+        elif action == 'F' or action == 'B':
+            flow = 'URDL'
+            is_opposite = (action == 'F' and not is_clockwise) or (action == 'B' and is_clockwise)
         else:
             assert False
         
@@ -183,41 +186,42 @@ class CRubiks(object):
             flow = flow[::-1]
 
         flow = flow[-1] + flow
-        new_vals = self.sides[self.sides_hash[flow[0]]].get_side(action)
-        for i in xrange(len(flow) - 1):
-            old_vals = self.sides[self.sides_hash[flow[i+1]]].get_side(action)
-            self.sides[self.sides_hash[flow[i+1]]].update_side(action, new_vals)
-            new_vals = old_vals
 
-        if rotated:
-            self.rotate_axis(rotated + "'")
+        new_vals = self.sides[self.sides_hash[flow[0]]].get_side(self.translate_rotate_action(action, flow[0]))
+        for i in xrange(len(flow) - 1):
+            old_vals = self.sides[self.sides_hash[flow[i+1]]].get_side(self.translate_rotate_action(action, flow[i+1]))
+            if action == 'B' and i in [0, 2]:
+                new_vals = new_vals[::-1]
+            if action in ['D', 'U'] and flow[i+1] in ['B']:
+                new_vals = new_vals[::-1]
+            self.sides[self.sides_hash[flow[i+1]]].update_side(self.translate_rotate_action(action, flow[i+1]), new_vals)
+            new_vals = old_vals
             
     def __repr__(self):
+    
         result = []
-        back_side = str(cube.sides[0])
-        vals = back_side.split('\n')
+
+        top_side = str(cube.sides[1])
+        vals = top_side.split('\n')
         for v in vals:
             result.append(' '* (len(v) + 3) + v)
         result.append('')
-        
+
         left_side = str(cube.sides[3])
         lvals = left_side.split('\n')
 
-        top_side = str(cube.sides[1])
-        tvals = top_side.split('\n')
+        front_side = str(cube.sides[2])
+        fvals = front_side.split('\n')
 
         right_side = str(cube.sides[4])
         rvals = right_side.split('\n')
-        
+
+        back_side = str(cube.sides[0])
+        bvals = back_side.split('\n')
+
         for i in xrange(len(lvals)):
-            sss = '%s   %s   %s' % (lvals[i], tvals[i], rvals[i])
+            sss = '%s   %s   %s   %s' % (lvals[i], fvals[i], rvals[i], bvals[i])
             result.append(sss)
-        result.append('')
-        
-        front_side = str(cube.sides[2])
-        vals = front_side.split('\n')
-        for v in vals:
-            result.append(' '* (len(v) + 3) + v)
         result.append('')
 
         down_side = str(cube.sides[5])
@@ -228,15 +232,13 @@ class CRubiks(object):
         return '\n'.join(result)
 
 
-cube = CRubiks()
-'''
-for i in xrange(6):
-    cube.rotate_single("R")
-    cube.rotate_single("U")
-    cube.rotate_single("R'")
-    cube.rotate_single("U'")
-#'''
+cube = CCube()
+
+#U B2 U B2 U B' U' R' U R B' U' B2
 
 print cube
-cube.rotate("R2 B2 U B2 U2 D' F2 D B2 R2 D' F' B' D F U2 R' U F' L' F2")
+cube.rotate("U B2 U B2")
 print cube
+
+
+
