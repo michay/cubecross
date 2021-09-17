d27e6f301047e9a6d965d537a2bccb760f59ae2b
myonin <micha.yonin@sandisk.com>
2021-09-17 15:52:18 +0300

    added canny edge detection

--- crossme/crossme/image_processing.h
+++ crossme/crossme/image_processing.h
@@ -1,4 +1,8 @@
 #pragma once
 
-__declspec(dllexport) int modify_frame(void* frame, int width, int height);
+__declspec(dllexport) int modify_frame(void* frame_p, int width, int height);
 
+
+
+
+__declspec(dllexport) int playground(void* frame_p, int width, int height);
